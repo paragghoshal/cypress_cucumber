29d81f143ceb00fb7596b31316055a347024dac9
paragghoshal <ghoshal.parag@gmail.com>
2021-12-30 15:47:05 +0530

    first commit

---
+++ cypress/integration/TestCases/login.spec.js
@@ -0,0 +1,12 @@
+describe("Login Suite",()=>{
+it("Login Test",()=>{
+
+cy.visit("/");
+cy.get("#user-name").type("standard_user");
+cy.get("#password").type("secret_sauce");
+cy.get("#login-button").click();
+cy.url().should("contains","/inventory.html");
+
+})
+
+})
